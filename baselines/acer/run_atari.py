9d011902d0cb37c0441924e0097391553184a225
zhangyx96 <17888825825@163.com>
2018-01-19 11:52:32 +0800

    more atari games

--- baselines/acer/run_atari.py
+++ baselines/acer/run_atari.py
@@ -40,7 +40,8 @@ def train(env_id, num_timesteps, seed, policy, lrschedule, num_cpu, perform, use
 def main():
     
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
+    #parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
+    parser.add_argument('--env', help='environment ID', default='AirRaidNoFrameskip-v4')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
     parser.add_argument('--lrschedule', help='Learning rate schedule', choices=['constant', 'linear'], default='constant')
